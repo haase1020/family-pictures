96dab20e529f089f88208599bebfb0538d473ae6
Mandi <haase1020@gmail.com>
2020-12-24 08:32:58 -0500

    finish basic functionality

--- paginate.js
+++ paginate.js
@@ -1,3 +1,12 @@
-const paginate = () => {};
+const paginate = (followers) => {
+  const itemsPerPage = 4;
+  const numberOfPages = Math.ceil(followers.length / itemsPerPage);
+  console.log(numberOfPages);
+  const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+  return newFollowers;
+};
 
 export default paginate;
